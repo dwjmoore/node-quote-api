eafb752be790e328266cf5e4bef443050f0f6890
David Moore <dwjmoore@gmail.com>
2023-05-04 21:39:32 +0000

    added post route for adding a quote

--- server.js
+++ server.js
@@ -26,6 +26,19 @@ app.get('/api/quotes', (req, res) => {
 	}
 });
 
+app.post('/api/quotes', (req, res) => {
+	const newQuote = {
+		quote: req.query.quote,
+		person: req.query.person
+	};
+	if (newQuote.quote && newQuote.person) {
+		quotes.push(newQuote);
+		res.send({ quote: newQuote });
+	} else {
+		res.status(400).send();
+	}
+});
+
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 })
